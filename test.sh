6a7f1213e93271ea5684edd45c6e4d07125738dc
Mark Willson <cdr.nil@gmail.com>
2025-04-03 12:52:26 +0100

    Make executable

--- test.sh
+++ test.sh
@@ -12,7 +12,7 @@ filter|x|x|smtp-in|data-line|x|x|Line 2
 filter|x|x|smtp-in|data-line|x|x|.
 EOF
 echo "-Address rewrite-"
-awk -f filter-rewrite-from.awk address no-reply@example.com <test.msgs
+./filter-rewrite-from.awk address no-reply@example.com <test.msgs
 cat >test.msgs << EOF
 config|ready
 filter|x|x|smtp-in|rcpt-to|x|x|user@example.com
@@ -25,7 +25,7 @@ filter|x|x|smtp-in|data-line|x|x|Line 2
 filter|x|x|smtp-in|data-line|x|x|.
 EOF
 echo "-Masquerade: won't rewrite-"
-awk -f filter-rewrite-from.awk masquerade example.com <test.msgs
+./filter-rewrite-from.awk masquerade example.com <test.msgs
 echo "-Masquerade: will rewrite-"
-awk -f filter-rewrite-from.awk masquerade other.com <test.msgs
+./filter-rewrite-from.awk masquerade other.com <test.msgs
 rm test.msgs
